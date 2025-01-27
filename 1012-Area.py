5ec0a4c2575f6a960986b34bbeb28c96dae4af92
SmDUm <samueldemoraesdelgado2005@gmail.com>
2025-01-27 17:53:32 -0300

    Exercícios

---
+++ 1012-Area.py
@@ -0,0 +1,16 @@
+A, B, C = input().split()
+A = float(A)
+B = float(B)
+C = float(C)
+
+TRIANGULO = (A*C)/2
+CIRCULO = 3.14159*C*C
+TRAPEZIO = ((A+B)*C)/2
+QUADRADO = B*B
+RETANGULO = A*B
+
+print(f'TRIANGULO: {TRIANGULO:.3f}')
+print(f'CIRCULO: {CIRCULO:.3f}')
+print(f'TRAPEZIO: {TRAPEZIO:.3f}')
+print(f'QUADRADO: {QUADRADO:.3f}')
+print(f'RETANGULO: {RETANGULO:.3f}')
