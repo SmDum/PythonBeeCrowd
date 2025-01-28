277a0c124d6bf438b7eca67d92ec1bb8cb75d6e3
SmDUm <samueldemoraesdelgado2005@gmail.com>
2025-01-28 19:54:35 -0300

    Exercícios

---
+++ 1015-DistanciaEntreDoisPontos.py
@@ -0,0 +1,14 @@
+from math import sqrt
+
+
+valor1, valor2 = input().split()
+valor1 = float(valor1)
+valor2 = float(valor2)
+
+valor3, valor4 = input().split()
+valor3 = float(valor3)
+valor4 = float(valor4)
+
+total = sqrt((valor3-valor1)**2 + (valor4-valor2)**2)
+
+print(f'{total:.4f}')
