996b683dfda5981f72676e91bb4102e6846fb02e
SmDUm <samueldemoraesdelgado2005@gmail.com>
2025-01-29 06:26:41 -0300

    Exercícios

---
+++ 1036-FórmulaDeBhaskara.py
@@ -0,0 +1,13 @@
+from math import sqrt
+
+A, B, C = map(float, input().split())
+delta = ((B * B) - 4 * A * C)
+
+if A == 0 or delta<0:
+    print('Impossivel calcular')
+    
+else:
+    R1 = (-B + sqrt(delta))/(2 * A)
+    R2 = (-B - sqrt(delta))/(2 * A)
+    print(f'R1 = {R1:.5f}')
+    print(f'R2 = {R2:.5f}')
